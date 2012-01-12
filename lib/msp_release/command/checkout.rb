722847acb514b023db7f82cf86417f21f45c1e2d
Nick Griffiths <nick@mediasp.com>
2012-01-12 21:13:48 +0000

    can specify a branch name to checkout latest release from

--- lib/msp_release/command/checkout.rb
+++ lib/msp_release/command/checkout.rb
@@ -9,39 +9,63 @@ module MSPRelease
 
     def run
       git_url = ARGV[1]
+      release_spec_arg = ARGV[2] || 'master'
+      pathspec = "origin/#{release_spec_arg}"
 
-      puts("Checking out latest release commit from #{git_url}...")
+      puts("Checking out latest release commit from #{pathspec}...")
 
       tmp_dir = "msp_release-#{Time.now.to_i}.tmp"
       Git.clone(git_url, {:out_to => tmp_dir, :exec => {:quiet => true}})
 
       project = Project.new(tmp_dir + "/" + Helpers::PROJECT_FILE)
 
-      src_dir = Dir.chdir(tmp_dir) {
+      src_dir = Dir.chdir(tmp_dir) do
 
-        all_commits = exec("git --no-pager log --no-color --full-index --pretty=oneline").
-        split("\n")
+        move_to(pathspec) unless pathspec == 'origin/master'
 
-        first_commit_hash, commit_message = all_commits.map { |commit_line|
-          match = /^([a-z0-9]+) (.+)$/i.match(commit_line)
-          [match[1], match[2]]
-        }.find {|hash, message|
-          project.release_name_from_message(message)
-        }
+        first_commit_hash, commit_message =
+          find_first_release_commit(project)
 
         if first_commit_hash.nil?
-          raise ExitException, "Could not find a release commit on master"
+          raise ExitException, "Could not find a release commit on #{pathspec}"
         end
 
         exec "git reset --hard #{first_commit_hash}"
         release_name = project.release_name_from_message(commit_message)
         project.source_package_name + "-" + release_name
-      }
+      end
 
       FileUtils.mv(tmp_dir, src_dir)
       puts("Checked out to #{src_dir}")
     end
 
+    private
+
+    def find_first_release_commit(project)
+      all_commits = exec("git --no-pager log --no-color --full-index --pretty=oneline").
+        split("\n")
+
+      all_commits.map { |commit_line|
+        match = /^([a-z0-9]+) (.+)$/i.match(commit_line)
+        [match[1], match[2]]
+      }.find {|hash, message|
+        project.release_name_from_message(message)
+      }
+    end
+
+    def move_to(pathspec)
+      begin
+        exec("git show #{pathspec} --")
+      rescue Exec::UnexpectedExitStatus => e
+        if /^fatal: bad revision/.match(e.stderr)
+          raise ExitException, "Git pathspec '#{pathspec}' does not exist"
+        else
+          raise
+        end
+      end
+
+      exec("git checkout --track #{pathspec}")
+    end
   end
 
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -20,7 +20,7 @@ describe 'checkout' do
         run_msp_release "checkout #{@remote_repo}"
 
         last_run.should exit_with(0)
-        last_stdout.should match("Checking out latest release commit from #{@remote_repo}...")
+        last_stdout.should match("Checking out latest release commit from origin/master...")
         last_stdout.should match("Checked out to project-0.0.1-rc1")
 
         File.directory?('project-0.0.1-rc1').should be_true
@@ -36,8 +36,49 @@ describe 'checkout' do
       in_tmp_dir do
         run_msp_release "checkout #{@remote_repo}"
         last_run.should exit_with(1)
-        last_stderr.should match("Could not find a release commit on master")
+        last_stderr.should match("Could not find a release commit on origin/master")
       end
     end
+  end
+
+  describe "checking out latest from a branch" do
+
+    before do
+      project = init_project('project', {:status => :RC})
+
+      in_project_dir do
+        run_msp_release 'branch'
+        run_msp_release 'new'
+        run_msp_release 'push'
+      end
+    end
+
+    it 'will checkout the lastest release on a branch if you pass branch:BRANCH_NAME as an argument' do
+
+      in_tmp_dir do
+        run_msp_release "checkout #{@remote_repo} release-0.0.1"
+
+        last_run.should exit_with(0)
+        last_stdout.should match("Checking out latest release commit from origin/release-0.0.1...")
+        last_stdout.should match("Checked out to project-0.0.1-rc1")
+
+        File.directory?('project-0.0.1-rc1').should be_true
+        Dir.chdir 'project-0.0.1-rc1' do
+          run_msp_release 'status'
+          last_stdout.should match('^Release commit: 0.0.1-rc1')
+          last_stdout.should match('^On release branch : 0.0.1')
+        end
+      end
+    end
+
+    it 'will fail if you give it a branch that does not exist' do
+      in_tmp_dir do
+        run_msp_release "checkout #{@remote_repo} release-0.1.0"
+
+        last_run.should exit_with(1)
+        last_stderr.should match("Git pathspec 'origin/release-0.1.0' does not exist")
+      end
+    end
+
   end
 end
