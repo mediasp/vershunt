5ae180810fcdeae11e52415c627cb651352e7e56
Nick Griffiths <nick@mediasp.com>
2012-01-13 18:24:25 +0000

    project spec passes

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,16 +3,29 @@ require 'spec/helpers'
 describe 'MSPRelease::Project' do
   include_context 'project_helpers'
 
-  describe 'reading the project file' do
+  before do
+    init_project('dev_project', {})
+  end
 
-    it 'can return the source package name of the project' do
-      project = write_project('dev_project', {
-        :changelog_path => 'debian/changelog',
-        :ruby_version_file => 'lib/my_project/version.rb'
-      })
+  let :project do
+    MSPRelease::Project.new('.msp_project')
+  end
 
+  it 'can return the source package name' do
+    in_project_dir do
       project.source_package_name.should == 'dev_project'
     end
+  end
 
+  it 'can return a changelog object' do
+    in_project_dir do
+      project.changelog.should_not be_nil
+    end
+  end
+
+  it 'can return the version of the project according to the ruby VERSION constant' do
+    in_project_dir do
+      project.version.should == MSPRelease::Version.new("0", "0", "1")
+    end
   end
 end
