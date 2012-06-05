45bdfb8760154f85b56fcd1a080d4993c70a2294
Nick Griffiths <nicobrevin@gmail.com>
2012-06-05 18:29:31 +0100

    move command line handling in to cli module

--- lib/msp_release.rb
+++ lib/msp_release.rb
@@ -152,59 +152,6 @@ module MSPRelease
 
   Author = Struct.new(:name, :email)
 
-  COMMANDS = ['new', 'push', 'branch', 'status', 'reset', 'bump', 'build', 'distrib', 'checkout']
-
-  def extract_global_args(args)
-    command_index = args.index {|a| /^[^\-]/.match(a) }
-    [args[0...command_index], args[command_index], args[command_index+1..-1]]
-  end
-
-  def run(args)
-    init_commands
-    # TODO there aren't any global options yet, so get rid?
-    global_args, cmd_name, command_args = extract_global_args(args)
-    options, leftovers = MSPRelease::Options.get(global_args)
-
-    cmd = @commands[cmd_name]
-
-    unless cmd
-      $stderr.puts("Unknown command: #{cmd_name}")
-      print_help
-      exit 1
-    end
-
-    begin
-      cmd.new(options, command_args).run
-    rescue ExitException => e
-      $stderr.puts("Command failed: #{e.message}")
-      exit e.exitstatus
-    rescue Exec::UnexpectedExitStatus => e
-      $stderr.puts("Command failed")
-      $stderr.puts("  '#{e.command}' exited with #{e.exitstatus}:")
-      $stderr.puts(e.stderr)
-      exit 1
-    end
-  end
-
-  def init_commands
-    @commands = {}
-    COMMANDS.each do |name|
-      require "msp_release/cli/#{name}"
-      camel_name =
-        name.split(/[^a-z0-9]/i).map{|w| w.capitalize}.join
-      @commands[name] = MSPRelease::CLI.const_get(camel_name)
-    end
-  end
-
-  def print_help
-    puts "Usage: msp_release COMMAND [OPTIONS]"
-    puts ""
-    puts "Available commands:"
-    COMMANDS.each do |cmd_name|
-      puts "  #{cmd_name.ljust(8)} #{@commands[cmd_name].description}"
-    end
-  end
-
   extend self
 
 end
--- lib/msp_release/cli.rb
+++ lib/msp_release/cli.rb
@@ -1,6 +1,7 @@
 module MSPRelease
-  class CLI
+  module CLI
 
+    # Base class for command line operations
     class Command
       include Exec::Helpers
 
@@ -22,6 +23,7 @@ module MSPRelease
       end
     end
 
+    # Commands that require a git working copy can include this module
     module WorkingCopyCommand
 
       include Helpers
@@ -42,5 +44,65 @@ module MSPRelease
       end
 
     end
+
+    # hardcoded list of commands
+    COMMANDS = ['new', 'push', 'branch', 'status', 'reset', 'bump', 'build', 'distrib', 'checkout']
+
+    # These are available on the CLI module
+    module ClassMethods
+
+      def extract_global_args(args)
+        command_index = args.index {|a| /^[^\-]/.match(a) }
+        [args[0...command_index], args[command_index], args[command_index+1..-1]]
+      end
+
+      def run(args)
+        init_commands
+        # TODO there aren't any global options yet, so get rid?
+        global_args, cmd_name, command_args = extract_global_args(args)
+        options, leftovers = MSPRelease::Options.get(global_args)
+
+        cmd = @commands[cmd_name]
+
+        unless cmd
+          $stderr.puts("Unknown command: #{cmd_name}")
+          print_help
+          exit 1
+        end
+
+        begin
+          cmd.new(options, command_args).run
+        rescue ExitException => e
+          $stderr.puts("Command failed: #{e.message}")
+          exit e.exitstatus
+        rescue Exec::UnexpectedExitStatus => e
+          $stderr.puts("Command failed")
+          $stderr.puts("  '#{e.command}' exited with #{e.exitstatus}:")
+          $stderr.puts(e.stderr)
+          exit 1
+        end
+      end
+
+      def init_commands
+        @commands = {}
+        COMMANDS.each do |name|
+          require "msp_release/cli/#{name}"
+          camel_name =
+            name.split(/[^a-z0-9]/i).map{|w| w.capitalize}.join
+          @commands[name] = MSPRelease::CLI.const_get(camel_name)
+        end
+      end
+
+      def print_help
+        puts "Usage: msp_release COMMAND [OPTIONS]"
+        puts ""
+        puts "Available commands:"
+        COMMANDS.each do |cmd_name|
+          puts "  #{cmd_name.ljust(8)} #{@commands[cmd_name].description}"
+        end
+      end
+    end
+
+    self.extend(ClassMethods)
   end
 end
