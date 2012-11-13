341decd18a18ad4bf9f3ce9ec9d4a8df69b97850
Nick Griffiths <nicobrevin@gmail.com>
2012-11-13 23:57:45 +0000

    totally change build command to be a checkout v2 command

--- lib/msp_release/cli/build.rb
+++ lib/msp_release/cli/build.rb
@@ -1,60 +1,171 @@
 module MSPRelease
   class CLI::Build < CLI::Command
+    include Debian::Versions
 
-    include CLI::WorkingCopyCommand
+    # When cloning repositories, limit to this many commits from each head
+    CLONE_DEPTH = 5
 
-    description "Build debian packages suitable for deployment"
+    description """Build debian packages suitable for deployment fresh from
+source control using a build commit or a development version.
+
+When no BRANCH_NAME is given, or that branch is not a release branch, the
+latest commit from that branch is checked out and the changelog version is
+adjusted to show this is a development build.
+
+If BRANCH_NAME denotes a release branch (i.e release-1.1) then the latest
+/release/ commit is checked out, even if there are commits after it.
+The changelog remains unaltered in this case - the source tree should have
+the correct version information in it.
+"""
+
+    arg :git_url, "URL used to clone the git repository"
+
+    arg :branch_name, "Name of a branch to build from, defaults to ${default}",
+    :default => "master"
+
+    opt :sign, "Pass options to dpkg-buildpackage to tell it whether or not to sign the build products",
+    {
+      :short => 'S',
+      :default => false
+    }
+
+    opt :shallow, "Only perform a shallow checkout to a depth of five" +
+      "commits from each head.  See git documentation for more details",
+    {
+      :short   => 's',
+      :default => true
+    }
+
+    opt :distribution, "Specify the debian distribution to put in the " +
+      "changelog when checking out a development version",
+    {
+      :short => 'd',
+      :long  => 'debian-distribution',
+      :type  => :string
+    }
+
+    opt :verbose, "Print some useful debugging output to stdout",
+    {
+      :long => 'verbose',
+      :short => 'v', :default => false
+    }
+
+    opt :noisy, "Output dpkg-buildpackage output to stderr",
+    {
+      :short => 'n', :default => false
+    }
+
+    opt :silent, "Do not print out build products to stdout", {
+      :default => false
+    }
 
     def run
-      fail_if_modified_wc
-      fail_unless_has_build_command
+      git_url          = arguments[:git_url]
+      release_spec_arg = arguments[:branch_name]
 
-      commit = git.latest_commit(project)
-      fail_unless_release_commit(commit)
+      do_build         = options[:build]
+      tar_it           = options[:tar]
+      clone_depth      = options[:shallow] ? CLONE_DEPTH : nil
 
-      build = Build.new('.', project)
-      build.perform_from_cli!
+      branch_name = release_spec_arg || 'master'
+      pathspec = "origin/#{branch_name}"
+      branch_is_release_branch = !! /^release-.+$/.match(branch_name)
+
+      shallow_output = clone_depth.nil?? '' : ' (shallow)'
+      if release_spec_arg && branch_is_release_branch
+        log("Checking out latest release commit from #{pathspec}#{shallow_output}")
+      else
+        log("Checking out latest commit from #{pathspec}#{shallow_output}")
+      end
+
+      tmp_dir = "vershunt-#{Time.now.to_i}.tmp"
+      Git.clone(git_url, {:depth => clone_depth, :out_to => tmp_dir,
+          :exec => {:quiet => true}})
+
+      project = Project.new_from_project_file(tmp_dir + "/" + Helpers::PROJECT_FILE)
+      distribution = options[:distribution] || project.changelog.distribution
+
+      src_dir = Dir.chdir(tmp_dir) do
+
+        if pathspec != "origin/master"
+          move_to(pathspec)
+        end
+
+        if branch_is_release_branch
+          first_commit_hash, commit_message =
+            find_first_release_commit(project)
+
+          if first_commit_hash.nil?
+            raise CLI::Exit, "Could not find a release commit on #{pathspec}"
+          end
+
+          exec "git reset --hard #{first_commit_hash}"
+        else
+          dev_version = Development.
+            new_from_working_directory(branch_name, latest_commit_hash)
+
+          project.changelog.amend(dev_version, distribution)
+        end
+        src_dir = project.source_package_name + "-" + project.changelog.version.to_s
+      end
+
+      FileUtils.mv(tmp_dir, src_dir)
+      project = Project.new_from_project_file(src_dir + "/" + Helpers::PROJECT_FILE)
+      log("Checked out to #{src_dir}")
+
+      log("Building package...")
+      out = options[:noisy] ? stderr : StringIO.new
+      build = Build.new(src_dir, project, :out => out, :sign => options[:sign])
+
+      result = build.perform_from_cli!
+      log("Build products:")
+      result.files.each {|f| stdout.puts(f) } unless options[:silent]
     end
 
     private
 
-    def available_changes_files
-      Dir["#{output_directory}/*.changes"]
+    def log(message)
+      stdout.puts(message) if options[:verbose]
     end
 
-    def find_changes_file(version)
-      available_changes_files.find { |fname|
-        (m = changes_pattern.match(fname)) && m && (m[1] == version)
+    def oneline_pattern
+      /^([a-z0-9]+) (.+)$/i
+    end
+
+    def log_command
+      "git --no-pager log --no-color --full-index"
+    end
+
+    def latest_commit_hash
+      output = exec(log_command + " --pretty=oneline -1").split("\n").first
+      oneline_pattern.match(output)[1]
+    end
+
+    def find_first_release_commit(project)
+      all_commits = exec(log_command +  " --pretty=oneline").
+        split("\n")
+
+      all_commits.map { |commit_line|
+        match = oneline_pattern.match(commit_line)
+        [match[1], match[2]]
+      }.find {|hash, message|
+        project.release_name_from_message(message)
       }
     end
 
-    def changes_pattern
-      /#{package_name}_([^_]+)_([^\.]+)\.changes/
-    end
-
-    def package_name
-      project.config[:deb_package_name]
-    end
-
-    def output_directory
-      @output_directory ||= File.expand_path(project.config[:deb_output_directory])
-    end
-
-    def build_command
-      project.config[:deb_build_command]
-    end
-
-    def fail_unless_release_commit(commit)
-      if ! commit.release_commit?
-        raise CLI::Exit, "HEAD is not a release commit:\n" +
-          "    #{commit.message[0...40]}..."
+    def move_to(pathspec)
+      begin
+        exec("git show #{pathspec} --")
+      rescue Exec::UnexpectedExitStatus => e
+        if /^fatal: bad revision/.match(e.stderr)
+          raise CLI::Exit, "Git pathspec '#{pathspec}' does not exist"
+        else
+          raise
+        end
       end
+
+      exec("git checkout --track #{pathspec}")
     end
 
-    def fail_unless_has_build_command
-      if build_command.nil? || build_command.empty?
-        raise CLI::Exit, "project does not define a build_command, can't build"
-      end
-    end
   end
 end
--- spec/build_spec.rb
+++ spec/build_spec.rb
@@ -3,112 +3,311 @@ require 'spec/helpers'
 describe 'build' do
   include_context 'project_helpers'
 
+  let :dev_version_regex do
+    "([0-9]{14}-git\\+[a-f0-9]{6}~([a-z\.]+))"
+  end
+
   before do
     # change this before you build_init_project
     @deb_exit_code = 0
   end
 
-  describe 'development builds' do
+  describe "no arguments given" do
+    it "fails, at least a repository arg is required"
   end
 
-  describe 'stable builds' do
-  end
-
-  it 'fails if no build command has been specified' do
-    project = build_init_project('project', {})
-    in_project_dir do
-      run_msp_release 'branch'
-      run_msp_release 'new'
-      run_msp_release 'push'
-      run_msp_release 'build'
-      last_run.should exit_with(1)
-      last_stderr.should include('project does not define a build_command')
+  def build_products
+    lines = last_stdout.split("\n")
+    if index = lines.index("Build products:")
+      # verbose output
+      lines[(index + 1)..-1]
+    else
+      lines
     end
   end
 
-  let :deb_options do
-    {:build_command => 'bin/build-debs.sh', :output_directory => '.',
-    :package_name => 'project'}
-  end
+  describe "only repository argument given" do
 
-  it 'fails if you are not commiting from a release commit' do
-    project = build_init_project('project', :deb => deb_options)
+    before do
+      build_init_project('project', {})
+    end
 
-    in_project_dir do
-      run_msp_release 'build'
-      last_run.should exit_with(1)
-      last_stderr.should include('HEAD is not a release commit')
+    it "checks out the latest commit from master then builds from it, giving " +
+      "the build a development version string" do
+
+      in_tmp_dir do
+        run_msp_release "build --verbose #{@remote_repo}"
+
+        last_run.should exit_with(0)
+        last_stdout.should match("Checking out latest commit from origin/master")
+        version_regex = /Checked out to project\-([0-9]{14}-git\+[a-f0-9]{6}~master)/
+        last_stdout.should match(version_regex)
+
+        package_built_regex =
+          /\/(project\_#{dev_version_regex}_[a-z0-9]+.changes)$/
+
+        build_products.any? {|f| package_built_regex.match(f)}.should be_true
+        build_products.each {|f| File.exists?(f).should be_true }
+
+        package_version = version_regex.match(last_stdout)[1]
+        full_package_name = 'project-' + package_version
+
+        File.directory?(full_package_name).should be_true
+        Dir.chdir full_package_name do
+          run_msp_release 'status'
+          last_run.should exit_with(0)
+          last_stdout.should include("Changelog says : #{package_version}")
+        end
+      end
+    end
+
+    it 'lets you specify a different distribution to the one in source control' do
+      in_tmp_dir do
+        run_msp_release "build --verbose --debian-distribution=tinternet #{@remote_repo}"
+
+        last_run.should exit_with(0)
+        last_stdout.should match("Checking out latest commit from origin/master")
+
+        checked_out_regex = /Checked out to project\-#{dev_version_regex}/
+        last_stdout.should match(checked_out_regex)
+        package_version = checked_out_regex.match(last_stdout)[1]
+
+        File.read("project-#{package_version}/debian/changelog").
+          first.strip.should ==
+          "project (#{package_version}) tinternet; urgency=low"
+      end
+    end
+
+    describe "in a working directory containing {project_name}_" do
+      it 'lets you checkout the latest from master and the builds it' do
+
+        in_tmp_dir 'project_builddir'  do
+          run_msp_release "build --verbose #{@remote_repo}"
+
+          last_run.should exit_with(0)
+          last_stdout.should match("Checking out latest commit from origin/master")
+
+          checked_out_regex = /Checked out to project\-#{dev_version_regex}/
+          last_stdout.should match(checked_out_regex)
+          package_version = checked_out_regex.match(last_stdout)[1]
+
+          package_built_regex = /\/(project\_#{dev_version_regex}_[a-z0-9]+.changes)/
+          last_stdout.should match(package_built_regex)
+
+          changes_fname = package_built_regex.match(last_stdout)[1]
+          File.exists?(changes_fname).should be_true
+        end
+      end
     end
   end
 
-  it 'fails if the build command fails' do
-    @build_extra = "echo 'spline diffraction imbalance' 1>&2 \nexit 1"
-    project = build_init_project('project', {
-      :deb => deb_options
-    })
+  describe "non-release branch name given" do
 
-    in_project_dir do
-      run_msp_release 'branch'
-      run_msp_release 'new'
-      run_msp_release 'push'
-      run_msp_release 'build'
+    before do
+      build_init_project('project', {})
 
-      last_run.should exit_with 1
-      last_stderr.should include('build failed:')
-      last_stderr.should include('spline diffraction imbalance')
+      in_project_dir do
+        exec("git branch feature-llama")
+        exec("git push origin feature-llama")
+      end
+    end
+
+    it "complains if the branch does not exist" do
+      in_tmp_dir do
+        run_msp_release "build --verbose  #{@remote_repo} feature-alpaca"
+        last_run.should exit_with(1)
+        last_stderr.should match("Git pathspec 'origin/feature-alpaca' does not exist")
+      end
+    end
+
+    it "checks out the branch and builds from HEAD, giving the build a spiffy" +
+      " development version including the branch info" do
+      in_tmp_dir do
+        run_msp_release "build --verbose  #{@remote_repo} feature-llama"
+        last_run.should exit_with(0)
+        last_stdout.should match("Checking out latest commit from origin/feature-llama")
+
+        checked_out_regex = /Checked out to project\-#{dev_version_regex}/
+        last_stdout.should match(checked_out_regex)
+        package_version = checked_out_regex.match(last_stdout)[1]
+        branch_part = checked_out_regex.match(last_stdout)[2]
+
+        branch_part.should == 'feature.llama'
+
+        package_built_regex =
+          /\/(project\_#{dev_version_regex}_[a-z0-9]+.changes)/
+        last_stdout.should match(package_built_regex)
+
+        changes_fname = package_built_regex.match(last_stdout)[1]
+        changes_fname.should include("feature.llama")
+        File.exists?(changes_fname).should be_true
+      end
     end
   end
 
-  it 'fails if no changes file results from the build' do
-    project = build_init_project('project', {
-      :deb => deb_options
-    })
+  describe "release branch name given" do
 
-    in_project_dir do
-      run_msp_release 'branch'
-      run_msp_release 'new'
-      run_msp_release 'push'
-      run_msp_release 'build'
-      last_run.should exit_with(1)
-      last_stderr.should include('Unable to find changes file')
+    before do
+      build_init_project('project', {})
+
+      in_project_dir do
+        run_msp_release 'branch'
+        run_msp_release 'new'
+        run_msp_release 'push'
+      end
+    end
+
+    it "complains if the branch does not exist" do
+      in_tmp_dir do
+        run_msp_release "build --verbose #{@remote_repo} release-2.0"
+        last_run.should exit_with(1)
+        last_stderr.should match("Git pathspec 'origin/release-2.0' does not exist")
+      end
+    end
+
+    it "checks out the latest release commit from the branch then builds it" do
+      in_tmp_dir do
+        run_msp_release "build --verbose #{@remote_repo} release-0.0"
+
+        last_run.should exit_with(0)
+        last_stdout.should match("Checking out latest release commit from origin/release-0.0")
+        last_stdout.should match("Checked out to project-0.0.1-1")
+
+        package_built_regex = /\/(project_0\.0\.1\-1_[a-z0-9]+\.changes)/
+        last_stdout.should match(package_built_regex)
+
+        changes_fname = package_built_regex.match(last_stdout)[1]
+        File.exists?(changes_fname).should be_true
+
+        File.directory?('project-0.0.1-1').should be_true
+        Dir.chdir 'project-0.0.1-1' do
+          run_msp_release 'status'
+          last_stdout.should match('^Release commit : 0.0.1-1')
+          last_stdout.should match('^Release branch : 0.0')
+        end
+      end
     end
   end
 
-  it 'fails if the resulting changes file does not match the expected version' do
+  describe "shallow clone" do
+    before do
+      build_init_project('project', {:deb =>
+          {:build_command => "dpkg-buildpackage -us -uc"}})
 
-    @build_extra = "touch project_0.1.0~1_all.changes"
+      in_project_dir do
+        # Create enough commits so that the first one will not show up in a
+        # shallow clone
+        (0..6).each do |iter|
+          exec("echo change >> dummy_file")
+          exec("git add dummy_file")
+          exec("git commit -m 'change #{iter}'")
 
-    project = build_init_project('project', {
-      :deb => deb_options
-    })
-
-    in_project_dir do
-      run_msp_release 'branch'
-      run_msp_release 'new'
-      run_msp_release 'push'
-      run_msp_release 'build'
-      last_run.should exit_with(1)
-      last_stderr.should include('Unable to find changes file with version: 0.0.1-1')
+        end
+        exec("git push origin master")
+      end
     end
+
+    it "will checkout the repository with short history (the default)" do
+      in_tmp_dir do
+        run_msp_release "build --verbose file:///#{@remote_repo}"
+        last_run.should exit_with(0)
+        last_stdout.should match("^Checking out latest commit from origin/master \\(shallow\\)$")
+        version_regex = /Checked out to project\-([0-9]{14}-git\+[a-f0-9]{6}~master)/
+        last_stdout.should match(version_regex)
+
+        package_version = version_regex.match(last_stdout)[1]
+        full_package_name = 'project-' + package_version
+
+        File.directory?(full_package_name).should be_true
+        Dir.chdir full_package_name do
+          run_msp_release 'status'
+          last_run.should exit_with(0)
+          last_stdout.should include("Changelog says : #{package_version}")
+          exec("git log")
+          last_stdout.should match("change 6")
+          last_stdout.should_not match("change 0")
+        end
+      end
+
+    end
+
+    it "can checkout the repository with full history" do
+      in_tmp_dir do
+        run_msp_release "build --verbose --no-shallow file:///#{@remote_repo}"
+        last_run.should exit_with(0)
+        last_stdout.should match("^Checking out latest commit from origin/master$")
+        version_regex = /Checked out to project\-([0-9]{14}-git\+[a-f0-9]{6}~master)/
+        last_stdout.should match(version_regex)
+
+        package_version = version_regex.match(last_stdout)[1]
+        full_package_name = 'project-' + package_version
+
+        File.directory?(full_package_name).should be_true
+        Dir.chdir full_package_name do
+          run_msp_release 'status'
+          last_run.should exit_with(0)
+          last_stdout.should include("Changelog says : #{package_version}")
+          exec("git log")
+          last_stdout.should match("change 6")
+          last_stdout.should match("change 0")
+        end
+      end
+
+    end
+
   end
 
-  it 'calls the build command, scanning for resulting build product' do
+  describe "distribution option" do
+  end
 
-    @build_extra = "touch project_0.0.1-1_all.changes"
+  describe "noise levels" do
 
-    project = build_init_project('project', {
-      :deb => deb_options
-    })
-
-    in_project_dir do
-      run_msp_release 'branch'
-      run_msp_release 'new'
-      run_msp_release 'push'
-      run_msp_release 'build'
-      last_run.should exit_with(0)
-      last_stdout.should match(/^Package built: (.+)project_0\.0\.1\-1_all\.changes$/)
+    before do
+      build_init_project('project', {})
     end
 
+    it "defaults to being quiet-ish" do
+
+      in_tmp_dir do
+        run_msp_release "build #{@remote_repo}"
+        last_run.should exit_with(0)
+
+        lines = last_stdout.split("\n")
+        lines.length.should == ["deb", "changes", "source tar", "dsc"].length
+      end
+    end
+
+    it "can be told to shut right up" do
+      in_tmp_dir do
+        run_msp_release "build --silent #{@remote_repo}"
+        last_run.should exit_with(0)
+
+        lines = last_stdout.strip.should == ""
+      end
+    end
+
+    it "will print some helpful output to stdout if you give --verbose" do
+      in_tmp_dir do
+        run_msp_release "build --verbose #{@remote_repo}"
+        last_run.should exit_with(0)
+
+        last_stdout.should match("Checking out latest commit from origin/master")
+        build_products.length.should == ["deb", "changes", "source tar", "dsc"].length
+      end
+    end
+
+    it "will print dpkg-buildpackage output to stderr if specified" do
+      in_tmp_dir do
+        run_msp_release "build --noisy #{@remote_repo}"
+        last_run.should exit_with(0)
+
+        lines = last_stdout.split("\n")
+        lines.length.should == ["deb", "changes", "source tar", "dsc"].length
+
+        last_stderr.strip.should_not == ""
+        last_stderr.should include("dpkg-buildpackage: source package project")
+      end
+    end
   end
 
 end
